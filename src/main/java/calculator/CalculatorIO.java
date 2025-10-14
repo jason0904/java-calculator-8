e6aed430380ef0450960537b6fbd341ae4b6a26b
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-14 15:26:37 +0000

    feat(CalculatorIO):입출력부 개발.

---
+++ src/main/java/calculator/CalculatorIO.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalculatorIO {
+
+    /*
+     * 계산기의 Input, Output을 담당.
+     */
+
+    private static final String Input_String = "덧셈할 문자열을 입력해 주세요.";
+    private static final String Output_String = "결과 : %d";
+
+    public String Input() {
+        
+        System.out.println(Input_String);
+        String inputString = Console.readLine();
+
+        return inputString;
+
+    }
+
+    public void Output(int result) {
+        System.out.printf(Output_String, result);
+    }
+
+}
