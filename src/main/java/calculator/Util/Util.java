70edc003879bd4914fc815c0eb38bb0c03055417
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-14 15:28:04 +0000

    fix(Util):굳이 클래스불러서 안써도되도록 static으로 함수 타입 변경.

--- src/main/java/calculator/Util/Util.java
+++ src/main/java/calculator/Util/Util.java
@@ -2,7 +2,7 @@ package calculator.Util;
 
 public class Util {
 
-    public boolean isInteger(char c) {
+    public static boolean isCharInteger(char c) {
         if(c - '0' >= 0 && c - '0' <= 9) return true;
         return false;
     }
