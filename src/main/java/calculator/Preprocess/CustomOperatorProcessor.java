2a58b0a7a95b29037f3a8f660b15d7cbeb9225ec
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-14 14:59:03 +0000

    feat(CustomOperatorProcessor): 커스텀 연산자가 들어왔는지 판단하는 기능 추가,  커스텀 오퍼레이터 저장하는 기능 추가와 예외처리 추가.

---
+++ src/main/java/calculator/Preprocess/CustomOperatorProcessor.java
@@ -0,0 +1,32 @@
+package calculator.Preprocess;
+
+public class CustomOperatorProcessor {
+
+    /*
+     * 커스텀 연산자 모드인지 여부 판단, 커스터 오퍼레이터 저장.
+     */
+
+    private char customOperator;
+
+    public char getCustomOperator() {
+        return customOperator;
+    }
+
+    public boolean checkCustomOperatorMode(String calculateString) {
+        if(calculateString.substring(0, 1).equals("//") && calculateString.substring(3,5).equals("\\n")) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setCustomOperator(char customOperator) throws IllegalArgumentException {
+        
+        if (customOperator - '0' >= 0 && customOperator - '0' <= 9) throw new IllegalArgumentException("커스텀 구분자가 숫자입니다.");
+
+        if (customOperator == ':' || customOperator == ',') throw new IllegalArgumentException("커스텀 구분자가 기본 구분자입니다.");
+
+        this.customOperator = customOperator;
+    }
+    
+
+}
