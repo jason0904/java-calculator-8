88f2572ef8dc0e2c3e12e38d1c81d7f312dd09d0
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-15 10:03:58 +0000

    feat(NumberPreprocessor):10이상의 숫자 들어왔을 경우 처리하는 기능 추가.

---
+++ src/main/java/calculator/Preprocess/NumberPreprocessor.java
@@ -0,0 +1,20 @@
+package calculator.Preprocess;
+
+public class NumberPreprocessor {
+
+    private StringBuilder numberStringBuilder = new StringBuilder("");
+
+    public void numberContinuousCharCalculate(char calculateChar) {
+        numberStringBuilder.append(calculateChar);
+    }
+
+    public Long returnNumber() {
+        return Long.parseLong(numberStringBuilder);
+    }
+    
+    public void resetTempNumber() {
+        numberStringBuilder.delete(0, numberStringBuilder.length());
+    }
+
+    
+}
