3f0eef5ae7ff0c853217726803a3208811b91dd6
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-15 14:25:25 +0000

    feat(CalculatorController):Input을 받아오고 계산결과를 표출하는 Controller 개발.

---
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -0,0 +1,15 @@
+package calculator.Controller;
+
+import calculator.Model.Calculator;
+import calculator.View.CalculatorIO;
+
+public class CalculatorController {
+    private Calculator calculator = new Calculator();
+    private CalculatorIO calculatorIO = new CalculatorIO();
+
+    public void run() {
+        String input = calculatorIO.Input();
+        Long result = calculator.calculate(input);
+        calculatorIO.Output(result);
+    }
+}
