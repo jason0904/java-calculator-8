e4ac237509a88344f889141d247e175ecad07e65
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-15 10:01:09 +0000

    feat(OperatorPreprocessor): 연산자인지 여부 판단하는 기능 추가.

---
+++ src/main/java/calculator/Preprocess/OperatorPreprocessor.java
@@ -0,0 +1,18 @@
+package calculator.Preprocess;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OperatorPreprocessor {
+
+    private CustomOperatorProcessor customOperatorProcessor = CustomOperatorProcessor.getInstance();
+    private final List<Character> operatorList = new ArrayList<>(Arrays.asList(':',','));
+    
+    public boolean isCharOperator(char calculateChar) {
+        if(customOperatorProcessor.getCheckCustomOperatorFlag() && customOperatorProcessor.getCustomOperator() == calculateChar) return true;
+        if(operatorList.contains(calculateChar)) return true;
+        return false;
+    }
+    
+}
