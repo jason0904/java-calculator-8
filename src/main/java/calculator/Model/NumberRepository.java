083bf22061e59c905565dd31280ed4f9e5d15e8a
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-17 14:21:20 +0000

    refactor(NumberRepository):NumberRepository를 일급컬렉션으로 만들기 위해, numbers에 final을 넣고 사본을 만들어서 return을 할수있도록 함.

--- src/main/java/calculator/Model/NumberRepository.java
+++ src/main/java/calculator/Model/NumberRepository.java
@@ -2,17 +2,24 @@ package calculator.Model;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
 
 public class NumberRepository {
 
-    private List<Long> numbers = new ArrayList<>();
+    /*
+     * 숫자를 저장하는 클래스.
+     */
+
+    private final List<Long> numbers = new ArrayList<>();
 
     public void addNumber(Long number) {
+        Objects.requireNonNull(number, "숫자 저장소에 null 숫자는 저장할 수 없습니다.");
         numbers.add(number);
     }
 
     public List<Long> getNumbers() {
-        return numbers;
+        return Collections.unmodifiableList(numbers);
     }
 
 }
