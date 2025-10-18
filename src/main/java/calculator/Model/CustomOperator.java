4168a39fa177e8850a2f4553065981fd6d3c274b
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-10-18 13:33:18 +0000

    refactor(CustomOperator):가독성 향상을 위해 if문 중괄호 추가, IllegalArugmentException은 RuntimeException이라 생성자에 throws 명시안해도된다고 하여 제거, 숫자확인로직 Character.isDigit사용으로 변경.

--- src/main/java/calculator/Model/CustomOperator.java
+++ src/main/java/calculator/Model/CustomOperator.java
@@ -7,9 +7,13 @@ public class CustomOperator {
 
     private final char customOperator;
 
-    public CustomOperator(char customOperator) throws IllegalArgumentException {
-        if (customOperator - '0' >= 0 && customOperator - '0' <= 9) throw new IllegalArgumentException("커스텀 구분자가 숫자입니다.");
-        if (customOperator == ':' || customOperator == ',') throw new IllegalArgumentException("커스텀 구분자가 기본 구분자입니다.");
+    public CustomOperator(char customOperator) {
+        if (Character.isDigit(customOperator)) {
+            throw new IllegalArgumentException("커스텀 구분자가 숫자입니다.");
+        }
+        if (customOperator == ':' || customOperator == ',') {
+            throw new IllegalArgumentException("커스텀 구분자가 기본 구분자입니다.");
+        }
         this.customOperator = customOperator;
     }
 
